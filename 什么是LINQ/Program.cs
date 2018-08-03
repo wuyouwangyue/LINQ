5e462cbec0617a256f829ab102dd1d5357e3672f
鑫 刘 <wuyou@lonlife.cn>
2018-08-03 18:13:39 +0800

    什么是LINQ

---
+++ 什么是LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 什么是LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 2, 12, 5, 15 };
+
+            var lowNumbers = from n in numbers where n < 10 select n;
+
+            foreach (var number in lowNumbers)
+            {
+                Console.WriteLine($"{number}");
+            }
+        }
+    }
+}
